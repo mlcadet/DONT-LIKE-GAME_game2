2503a836c50f34f6bd2689f82e9afbd03ea43350
Lucie Cadet <cadet.ml@gmail.com>
2024-12-20 17:33:38 -0500

    Setting class Constr

--- app.js
+++ app.js
@@ -1,26 +1,52 @@
 //Setting the Alien STUGAME object
 let alienStuGame = {
     name: "Alien StuGame",
-    hull: -20,
-    firePower: -5,
-    accuracy: -0.7,
+    hull: 20,
+    firePower: 5,
+    accuracy: 0.7,
     isAlive: true,
-    attack(target) {
-        
+    attack() {                             //add another property attack
+        let attackChance = Math.random();  //To determine accuracy
+        if (attackChance <= this.accuracy) {
+            return true;
+        } else {
+            return false;
+                }
+            },
+        };
+   
+
+// console.log('attack');
+
+
+//Setting the Class Alien Ship objects with constructor
+class AlienShip {
+    constructor (name, hull, firePower, accuracy) {
+        this.name = name;
+        this.hull = hull;
+        this.firePower = firePower;
+        this.accuracy = accuracy;
+
+        console.log('name:' + name)
     }
 }
-
-console.log('attack');
-//     attack() {
-//         let attachChance = Math.random();
-//         if (attachChance <= this.accuracy) {
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     },
-// };
-
+let alienShip = new AlienShip();        //Create a new instance of AlienShip (AS)
 
 
 //Setting the Alien Ship objects
+
+
+
+
+
+
+
+// attack() {
+//     let attackChance = Math.random();  //To determine accuracy
+//     if (attackChance <= this.accuracy) {
+//         return true;
+//     } else {
+//         return false;
+//             }
+//         },
+//     };
