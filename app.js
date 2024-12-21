be670adf51a648cbb17c84b489303591cc38336e
Lucie Cadet <cadet.ml@gmail.com>
2024-12-21 13:21:01 -0500

    trying diff destroyer

--- app.js
+++ app.js
@@ -25,35 +25,64 @@ class AlienShip {
         this.name = name;               //use the this keyword to refer to the instance being created
         this.hull = hull;
         this.firePower = firePower;
-        this.accuracy = accuracy;
-        
-       
+        this.accuracy = accuracy;    
+    }
+    attack() {
+        let accuracyCheck = Math.random();  //Math.radom checking accuracy - if the attack hits the target ship based on accuracy
+        if (accuracyCheck <= this.accuracy) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
-let alienShip = new AlienShip('ship1', 'survive');        //Create a new instance of AlienShip (AS)
-console.log('alienShip: ' + alienShip.name + ' ' + alienShip.hull);
 
-let alienShip2 = new AlienShip('ship2', 'survive');       //Static Property
-console.log(alienShip.name + ' ' + alienShip2.name);
+class Ship {
+    constructor (hull, firePower, accuracy) {
+        this.hull = hull;
+        this.firePower = firePower;
+        this.accuracy = accuracy;
+
+    }
+    attack(targetShip) {
+        if (Math.random() <= this.accuracy) {    //use the this keyword to refer to the instance being created
+            targetShip.hull -= this.firePower;   //reduce target ship hull by firepower
+            console.log("Attack successful!");
+            console.log(`Target ship hull: ${targetShip.hull}`);
+            
+        } else {
+            console.log("Attack missed!");
+            
+        }
+    }
+}
+
+// let alienShip = new AlienShip('ship1', 'survive');        //Create a new instance of AlienShip (AS)
+// console.log('alienShip: ' + alienShip.name + ' ' + alienShip.hull);
+
+
+// alienShip.maxHeight = 2000;  //adding property to a class
+
+// let alienShip2 = new AlienShip('ship2', 'ship2');       //Static Property or class property
+// console.log(alienShip.name + ' ' + alienShip2.name);
+// console.log(alienShip.maxHeight);
 
 
 
+
+
+//Using "extends" keyword to inherit all methods from another class
+class PlayerShip extends Ship {
+
+
+}
+
+
+class AlienShip extends Ship {
+    get
+}
 
 
 //Setting the Alien Ship objects
 
 
-
-
-
-
-
-// attack() {
-//     let attackChance = Math.random();  //To determine accuracy
-//     if (attackChance <= this.accuracy) {
-//         return true;
-//     } else {
-//         return false;
-//             }
-//         },
-//     };
